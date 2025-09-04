62436873c706e23dd1816a1531ff22a72627eb84
msuleman984 <muhammad.suleman@codbeyon.com>
2025-09-05 04:20:09 +0500

    updated email template

--- services/email.go
+++ services/email.go
@@ -10,9 +10,12 @@ func SendEmail(cfg config.Config, to, subject, body string) error {
 	from := cfg.SMTPEmail
 	pass := cfg.SMTPPassword
 
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: " + subject + "\n\n" +
+	msg := "From: " + from + "\r\n" +
+		"To: " + to + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"MIME-Version: 1.0\r\n" +
+		"Content-Type: text/plain; charset=\"utf-8\"\r\n" +
+		"\r\n" +
 		body
 
 	err := smtp.SendMail("smtp.gmail.com:587",
